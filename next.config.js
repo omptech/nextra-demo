9030b58e10ba45d58eb89277a91e20ee39015024
Omprakash Lodhi <omprakash.lodhi@nagarro.com>
2024-02-15 02:40:10 +0530

    test

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx'
+})
+
+module.exports = withNextra({
+    // Your existing configuration and new conditional paths
+    reactStrictMode: true,
+    basePath: isProd ? '/nextra-demo' : '',
+    assetPrefix: isProd ? '/nextra-demo/' : '',
+});
