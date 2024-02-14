3c7f753f7f5443b5da55d3b602af3e73b75c9329
Omprakash Lodhi <omprakash.lodhi@nagarro.com>
2024-02-15 03:32:17 +0530

    Updates

---
+++ _next/static/aZerNIBddTbU5YjTZhIxi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/445c4173ffc2bf04.css","static/chunks/64-936a71c06c305b42.js","static/chunks/pages/index-6f479880558f0939.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
