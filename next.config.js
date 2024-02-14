6687e092a7da98235a9c2bd9b51b3c3c78bbbd34
Omprakash Lodhi <omprakash.lodhi@nagarro.com>
2024-02-15 03:04:26 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
-const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
-})
-
-module.exports = {
-    // Your existing configurations
+const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
+module.exports = withNextra({
     reactStrictMode: true,
-    // Add the export configuration
     output: 'export',
-    // Your basePath and assetPrefix configurations
-    basePath: process.env.NODE_ENV === 'production' ? '/nextra-demo' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/nextra-demo/' : '',
-};
+})
