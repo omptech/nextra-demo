69d5cedf9816b76ec08d4d3bb41407fc614d4b20
omptech <55268151+omptech@users.noreply.github.com>
2024-02-15 16:06:50 +0530

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// pages/_app.js
+import 'nextra-theme-docs/style.css' // Adjust the theme according to your choice
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps }
+    />
+}
