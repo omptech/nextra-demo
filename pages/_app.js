db54b69fb331d06e753d98542966e1722a8a74b5
omptech <55268151+omptech@users.noreply.github.com>
2024-02-15 16:12:11 +0530

    Delete pages/_app.js

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-// pages/_app.js
-import 'nextra-theme-docs/style.css' // Adjust the theme according to your choice
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps }
-    />
-}
