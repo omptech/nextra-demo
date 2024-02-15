8f9f864094acb2387bad7d4baa4ff9870837ce34
omptech <55268151+omptech@users.noreply.github.com>
2024-02-15 11:43:47 +0000

    Deploying to gh-pages from @ omptech/nextra-demo@98bef9a0b25fd2439f13be57e5720951d04f1417 🚀

--- _next/static/SuyxtgGfw2G89jbgRJaBv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/545-3a91f0309f52c8ad.js","static/chunks/pages/index-1b6b960e3c002ae0.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
