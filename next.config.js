2a41d6840e5e47074b8a86982a4a2c2bf6a8fa87
omptech <55268151+omptech@users.noreply.github.com>
2024-03-04 17:23:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 const isProd = process.env.NODE_ENV === 'production'
-const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
-})
+/** @type {import('next').NextConfig} */
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-
-module.exports = withNextra({
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  assetPrefix,
+  basePath: assetPrefix,
+  output: "export",
+};
+module.exports = {
     basePath: process.env.PR_NUMBER ? `/pr-${process.env.PR_NUMBER}` : '',
     assetPrefix: process.env.PR_NUMBER ? `/pr-${process.env.PR_NUMBER}` : '',
     images: {
@@ -14,4 +25,6 @@ module.exports = withNextra({
     output: "export",
     // Your existing configuration and new conditional paths
     reactStrictMode: true
-});
+    ...withNextra(),
+    ...nextConfig,
+};
